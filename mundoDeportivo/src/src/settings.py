f7688499cd832fcfa8ecefa3417c00819567b74c
Fernando Méndez Requena <nandomr92@gmail.com>
2017-01-14 01:09:20 +0100

    Update settings.py

--- mundoDeportivo/src/src/settings.py
+++ mundoDeportivo/src/src/settings.py
@@ -29,8 +29,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST='smtp.gmail.com'
-EMAIL_HOST_USER= 'correopruebaaii@gmail.com'
-EMAIL_HOST_PASSWORD = 'correoprueba'
+EMAIL_HOST_USER= ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
